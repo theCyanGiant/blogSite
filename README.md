83b0a51106cf8472b44652d79b39a0b74926cb38
James Bennett <J.bennett@gmx.com>
2015-08-27 16:18:55 -0700

    updates to render blog class live from parse.com, updates readme

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # blogSite
 Making a Blog site!
+<p> V 0.0.3
+<p> Added Blog class via parse.com and renders it to the site. Needs author and time/date tags to render. 
 <p> V 0.0.1
-<p> Standard Blog template sourced from [bootstrap](http://getbootstrap.com/examples/blog/#).  Links not functional except social media. 
+<p> Standard Blog template sourced from [bootstrap](http://getbootstrap.com/examples/blog/#).  Links not functional except social media. Parse.JS provides db server, github pages provides web server.
--- js/blog.js
+++ js/blog.js
@@ -5,10 +5,32 @@ $(function() {
     // Replace this line with the one on your Quickstart Guide Page
     Parse.initialize("pIt3SBZJ7g2eQrNtJXc7LpkHAWHpJ0d1REpPgD9C", "4j8T2wcY7Y2aCOVDB8W3axJdTZDPLXRhz18gu0X7");
 
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("yay! it worked");
-    });
+    // var TestObject = Parse.Object.extend("TestObject");
+    // var testObject = new TestObject();
+    // testObject.save({foo: "bar"}).then(function(object) {
+    //   alert("yay! it worked");
+    // });
 
+    var Blog  = Parse.Object.extend("Blog");
+    var Blogs = Parse.Collection.extend({
+      model: Blog
+    });
+    var blogs = new Blogs();
+    blogs.fetch({
+      success: function(blogs){
+        var blogsView = new BlogsView({ collection: blogs});
+        blogsView.render();
+        $('.main-container').html(blogsView.el);
+      },
+      error: function(blogs, error) {
+        console.log(error);
+      }
+    });
+    var BlogsView  = Parse.View.extend({
+      template: Handlebars.compile($('#blogs-tpl').html()),
+      render: function(){
+        var collection = { blog: this.collection.toJSON() };
+        this.$el.html(this.template(collection));
+      }
+    })
 });
